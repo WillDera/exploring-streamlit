f0d89570ba8fdb1e8e37c8622f2b9c43ec9026f0
Godswill <30760648+WillDera@users.noreply.github.com>
2020-07-18 23:51:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,22 @@ st.title("Streamlit Kami")
 # Header/Subheader
 st.header("The kami header")
 st.subheader("The kami subheader")
+
+# Normal Text
+st.text("Hello from kami")
+
+# Markdown
+st.markdown("### This is a kamidown")
+
+# Error/Colourful text
+st.success("Kami sucess")
+st.info("Kamisoooo")
+st.warning("Be damned")
+
+# Get help Info about Python range
+st.help(range)
+
+# Writing text
+st.write("Text with kami")
+
+st.write(range(10))
